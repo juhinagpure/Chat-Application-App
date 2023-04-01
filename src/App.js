689c076bff214cfb83d445505008d94cd8f8e769
JUHI NAGPURE <juhinagpure143@gmail.com>
2023-04-01 20:23:49 +0530

    added height,projectID,userName and userSecret from chat engine

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import {chatEngine} from 'react-chat-engine';
+import {ChatEngine} from 'react-chat-engine';
 
 import './App.css';
+
 const App = () => {
-    return (
-        <chatEngine
-height="100vh"
-projectID="70a46fa4-8497-48fb-8f20-fac1e58d7aaf"
-userName=""
-userSecret=""
-        />
-    )
+return(
+    <ChatEngine
+       height="100vh"
+       projectID="70a46fa4-8497-48fb-8f20-fac1e58d7aaf"
+       userName="juhinagpure143"
+       userSecret="123123"
+    />
+)
 }
 
 export default App;
