c70305c48966e7425809a1f1c7c4645e87a077ad
JUHI NAGPURE <juhinagpure143@gmail.com>
2023-04-01 17:55:47 +0530

    added details by using chat Engine

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import {chatEngine} from 'react-chat-engine';
+
+import './App.css';
+const App = () => {
+    return (
+        <chatEngine
+height="100vh"
+projectID="70a46fa4-8497-48fb-8f20-fac1e58d7aaf"
+userName=""
+userSecret=""
+        />
+    )
+}
+
+export default App;
