e6354a80e8aa85a487f0bf09366702ab64656c6a
JUHI NAGPURE <juhinagpure143@gmail.com>
2023-04-02 11:38:43 +0530

    made some changes in app.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import {ChatEngine} from 'react-chat-engine';
+import { ChatEngine } from "react-chat-engine";
 
-import './App.css';
 
 const App = () => {
-return(
-    <ChatEngine
-       height="100vh"
-       projectID="70a46fa4-8497-48fb-8f20-fac1e58d7aaf"
-       userName="juhinagpure143"
-       userSecret="123123"
-    />
-)
-}
+    return (
+        <ChatEngine 
+           height="100vh"
+           projectID="a29ef303-622d-4e0e-954f-79cc81be8de7"
+           userName="Juhi123"
+           userSecret="123123"
 
-export default App;
+        />
+    )
+}
